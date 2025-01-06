221dd94af12b26fcfee41d890b02b9b7ca39a84a
pnilsssson <p.nilsson95@hotmail.com>
2025-01-06 21:43:03 +0100

    Removed unused import.

--- src/app/(authenticated)/program/actions.ts
+++ src/app/(authenticated)/program/actions.ts
@@ -12,7 +12,6 @@ import {
   ActionResponse,
   ConfigurationResponse,
   ProgramResponse,
-  ProgramTinyResponse,
 } from "@/types/types";
 import { revalidatePath } from "next/cache";
 import { openai } from "@ai-sdk/openai";
