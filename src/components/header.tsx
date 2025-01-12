8a2707ae373d947439336e586aefe0e60555d997
pnilsssson <p.nilsson95@hotmail.com>
2025-01-12 16:22:38 +0100

    Simplified header.

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,10 +1,5 @@
 "use client";
 
-import {
-  NavigationMenu,
-  NavigationMenuList,
-  NavigationMenuItem,
-} from "./ui/navigation-menu";
 import { Button } from "./ui/button";
 import Link from "next/link";
 import { EnterIcon } from "@radix-ui/react-icons";
@@ -13,23 +8,17 @@ import { usePathname } from "next/navigation";
 export default function Component() {
   const pathname = usePathname();
   return (
-    <header className="w-full bg-background flex items-center justify-between px-4 md:px-6">
-      <NavigationMenu className="">
-        <NavigationMenuList className="py-4 w-full">
-          <NavigationMenuItem className="text-lg font-medium">
-            <Button variant="link" className="text-lg px-0 font-medium">
-              <Link href="/">Vivotiv</Link>
-            </Button>
-          </NavigationMenuItem>
-          {pathname.startsWith("/auth") ? null : (
-            <Button asChild className="!ml-auto">
-              <Link href="/auth/signin">
-                <EnterIcon className="h-5 w-5" /> Sign in
-              </Link>
-            </Button>
-          )}
-        </NavigationMenuList>
-      </NavigationMenu>
+    <header className="flex justify-between p-4">
+      <Button variant="link" className="text-lg px-0 font-medium">
+        <Link href="/">Vivotiv</Link>
+      </Button>
+      {pathname.startsWith("/auth") ? null : (
+        <Button asChild className="!ml-auto">
+          <Link href="/auth/signin">
+            <EnterIcon className="h-5 w-5" /> Sign in
+          </Link>
+        </Button>
+      )}
     </header>
   );
 }
