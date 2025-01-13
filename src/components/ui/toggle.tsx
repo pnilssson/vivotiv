08570fa9aa6888e612f8f1c63688b2b203c1f747
Pär Nilsson <p.nilsson95@hotmail.com>
2025-01-13 09:51:10 +0100

    Reset button and toggle sizes.

--- src/components/ui/toggle.tsx
+++ src/components/ui/toggle.tsx
@@ -16,9 +16,9 @@ const toggleVariants = cva(
           "border border-input bg-transparent hover:bg-accent hover:text-accent-foreground",
       },
       size: {
-        default: "h-8 px-3",
-        sm: "h-7 px-2",
-        lg: "h-9 px-5",
+        default: "h-10 px-3",
+        sm: "h-9 px-2.5",
+        lg: "h-11 px-5",
       },
     },
     defaultVariants: {
