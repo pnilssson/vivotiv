f4c4ec27c8bcf74412fabd9a910bdb74225d4e7c
pnilsssson <p.nilsson95@hotmail.com>
2025-01-22 23:14:36 +0100

    Made heading title smaller.

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -13,7 +13,7 @@ export default function Component() {
     <SectionContent className="container p-4 sticky top-0 z-50">
       <ContentBox className="flex flex-row justify-between px-4 py-2">
         <Button variant="link" className="p-0" size="sm">
-          <Title>
+          <Title className="text-xl">
             <Link href="/">Vivotiv</Link>
           </Title>
         </Button>
